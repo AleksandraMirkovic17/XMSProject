4b9835ec035f2ff692c6af5f65c00ec1ceb59301
FedorBozic <fedorbozic@gmail.com>
2022-06-23 20:38:38 +0200

    refactor: Removed old maven project

--- userservice/src/main/java/com/xws/userservice/enums/EGender.java
+++
@@ -1,6 +0,0 @@
-package com.xws.userservice.enums;
-
-public enum EGender {
-	MALE,
-	FEMALE
-}
--- userservice/src/main/java/com/xws/userservice/repository/RoleRepository.java
+++
@@ -1,9 +0,0 @@
-package com.xws.userservice.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.xws.userservice.model.Role;
-
-public interface RoleRepository extends JpaRepository<Role, Integer> {
-
-}
