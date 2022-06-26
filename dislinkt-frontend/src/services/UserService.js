afb92a4c0a393683b90a5614b540eb1dc369526e
FedorBozic <fedorbozic@gmail.com>
2022-06-26 20:06:38 +0200

    feat: Initial frontend-backend integration

---
+++ dislinkt-frontend/src/services/UserService.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+const USER_API_BASE_URL = 'http://localhost:4200/user';
+
+class UserService{
+
+    getUsers(){
+        return axios.get(USER_API_BASE_URL);
+    }
+
+    registerUser(user){
+        return axios.post(USER_API_BASE_URL, user);
+    }
+}
+
+export default new UserService();
--- dislinkt-frontend/src/views/Registration.vue
+++ dislinkt-frontend/src/views/Registration.vue
@@ -7,31 +7,31 @@
     <br>
     <div class="input-group mb-3">
       <span class="input-group-text" id="basic-addon1">Name</span>
-      <input type="text" class="form-control" placeholder="Name" aria-label="Name" aria-describedby="basic-addon1">
+      <input type="text" class="form-control" placeholder="Name" aria-label="Name" aria-describedby="basic-addon1" v-model="user.name">
     </div>
     <div class="input-group mb-3">
       <span class="input-group-text" id="basic-addon2">Surname</span>
-      <input type="text" class="form-control" placeholder="Surname" aria-label="Surname" aria-describedby="basic-addon1">
+      <input type="text" class="form-control" placeholder="Surname" aria-label="Surname" aria-describedby="basic-addon1" v-model="user.surname">
     </div>
     <div class="input-group mb-3">
       <span class="input-group-text" id="basic-addon3">Username</span>
-      <input type="text" class="form-control" placeholder="Userrname" aria-label="Username" aria-describedby="basic-addon1">
+      <input type="text" class="form-control" placeholder="Userrname" aria-label="Username" aria-describedby="basic-addon1" v-model="user.username">
     </div>
     <div class="input-group mb-3">
       <span class="input-group-text" id="basic-addon4">Email</span>
-      <input type="text" class="form-control" placeholder="email@example.com" aria-label="Email" aria-describedby="basic-addon1">
+      <input type="text" class="form-control" placeholder="email@example.com" aria-label="Email" aria-describedby="basic-addon1" v-model="user.email">
     </div>
     <div class="input-group mb-3">
       <span class="input-group-text" id="basic-addon5">Phone number</span>
-      <input type="text" class="form-control" placeholder="838942868109041" aria-label="Surname" aria-describedby="basic-addon1">
+      <input type="text" class="form-control" placeholder="838942868109041" aria-label="Surname" aria-describedby="basic-addon1" v-model="user.contactPhone">
     </div>
     <div class="input-group mb-3">
       <span class="input-group-text" id="basic-addon6">Date of birth</span>
-      <input type="date" class="form-control"  aria-describedby="basic-addon1">
+      <input type="date" class="form-control"  aria-describedby="basic-addon1" v-model="user.date">
     </div>
     <div class="input-group mb-3">
       <span class="input-group-text" id="basic-addon7">Gender</span>
-      <select class="form-select" aria-label="Default select example">
+      <select class="form-select" aria-label="Default select example" v-model="user.gender">
         <option selected>Select</option>
         <option value="male">Male</option>
         <option value="female">Female</option>
@@ -40,14 +40,14 @@
     </div>
     <div class="input-group mb-3">
       <span class="input-group-text" id="basic-addon8">Password</span>
-      <input type="password" class="form-control" aria-label="Surname" aria-describedby="basic-addon1">
+      <input type="password" class="form-control" aria-label="Surname" aria-describedby="basic-addon1" v-model="user.password">
     </div>
     <div class="input-group mb-3">
       <span class="input-group-text" id="basic-addon9">Repeat password</span>
       <input type="password" class="form-control" aria-label="Surname" aria-describedby="basic-addon1">
     </div>
     <br>
-    <button type="button" class="btn btn-dark" style="width: 100%; height: 60px">Register</button>
+    <button type="button" class="btn btn-dark" style="width: 100%; height: 60px" v-on:click="registerUser()">Register</button>
     <br>
     <p style="font-size: 12pt">
       Already have an account? <a href="/login">Log in.</a>
@@ -60,8 +60,21 @@
 </template>
 
 <script>
+
+import UserService from '../services/UserService';
+
 export default {
-  name: "Registration"
+  name: "Registration",
+  data(){
+      return {
+          user: {}
+      }
+  },
+  methods: {
+      registerUser(){
+          UserService.registerUser(this.user);
+      }
+  }
 }
 </script>
 
