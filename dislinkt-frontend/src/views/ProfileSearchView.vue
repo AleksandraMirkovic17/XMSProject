fe85e3e1fc4fe2782e3ef6e057ef11748ddb7bf8
Andrea Sabo Cibolja <andrea_saboc@uns.ac.rs>
2022-07-01 08:59:13 +0200

    feat: some changes

--- dislinkt-frontend/src/views/ProfileSearchView.vue
+++ dislinkt-frontend/src/views/ProfileSearchView.vue
@@ -1,7 +1,17 @@
 <template>
   <div>
-    <div v-for="user in users" :key="user.name">
-      <p>nene</p>
+    <div v-for="(user,index) in users" :key="index">
+      <div class="profile-container">
+        <div class="profile-icon">
+          <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-person-fill" viewBox="0 0 16 16">
+            <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/>
+          </svg>
+        </div>
+        <div class="info">
+          <h4>{{user.name}} {{user.surname}}</h4>
+          <h3>{{user.username}}</h3>
+        </div>
+      </div>
 
     </div>
   </div>
@@ -18,7 +28,7 @@ export default {
     }
     },
   mounted() {
-    var searchParams = this.$route.params.search
+    //var searchParams = this.$route.params.search
     console.log(UserService.getUsers())
     UserService.getUsers().then(res => {
       this.users = res.data.users
@@ -33,5 +43,14 @@ export default {
 </script>
 
 <style scoped>
+.profile-container{
+  border-radius: 20px;
+  backround-color: white;
+  color: #e5e5e5;
+  display: flex;
+  flex-direction: row;
+  horiz-align: center;
+  vertical-align: center;
+}
 
 </style>
