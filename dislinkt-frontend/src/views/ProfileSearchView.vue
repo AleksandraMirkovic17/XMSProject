a2f1d4a0dfaa2cb83d528237a6e18199a9e0cd99
Andrea Sabo Cibolja <andrea_saboc@uns.ac.rs>
2022-07-04 19:06:06 +0200

    feat: redirect from search to profile

--- dislinkt-frontend/src/views/ProfileSearchView.vue
+++ dislinkt-frontend/src/views/ProfileSearchView.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="profiles-view">
     <div v-for="(user,index) in users" :key="index">
-      <div class="profile-container">
+      <div class="profile-container" v-on:click="redirectToProfile(user)" style="cursor: pointer">
         <div class="profile-icon">
           <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-person-fill" viewBox="0 0 16 16">
             <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/>
@@ -36,6 +36,9 @@ export default {
 
   },
   methods:{
+    redirectToProfile(user){
+      this.$router.push("/profile/"+user.username)
+    }
 
   }
 }
