6360be91eb2da7397b6446e5274a48eba15f52cf
AleksandraMirkovic17 <mirkovicaleksandra@outlook.com>
2022-08-23 15:25:18 +0200

    feat: add script.sql

---
+++ AgentskaApp/skripta.sql
@@ -0,0 +1,24 @@
+INSERT INTO public.user_table (
+id, activated, email, password, username) VALUES (
+'500', true, 'a@gmail.com', '$2a$04$Vbug2lwwJGrvUXTj6z7ff.97IzVBkrJ1XfApfGNl.Z695zqcnPYra', 'neki tamo');
+
+INSERT INTO public.user_table (
+id, activated, email, password, username) VALUES (
+'501', true, 'alexmirkovic1719@gmail.com', '$2a$04$Vbug2lwwJGrvUXTj6z7ff.97IzVBkrJ1XfApfGNl.Z695zqcnPYra', 'alexmirkovic1719');
+
+INSERT INTO public.role(
+id, name) VALUES (
+1,'ADMINISTRATOR');
+
+INSERT INTO public.role(
+id, name) VALUES (
+2,'USER');
+
+INSERT INTO public.role(
+id, name) VALUES (
+3,'OWNER');
+
+
+
+
+
--- AgentskaApp/src/main/resources/data.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO public.role (
-id, name) VALUES (
-500, 'ADMINISTRATOR');
